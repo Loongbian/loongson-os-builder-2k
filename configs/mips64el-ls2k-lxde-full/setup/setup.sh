f005f8a76b6314d642cc0a3ca403205aa051b03b
Peter <mbyzhang@outlook.com>
2021-01-10 11:47:16 +0800

    ls2k-lxde: fix incomplete lxde installation

--- configs/mips64el-ls2k-lxde-full/setup/setup.sh
+++ configs/mips64el-ls2k-lxde-full/setup/setup.sh
@@ -8,6 +8,6 @@ basic_setup
 enable_splash
 run_loongbian_workarounds
 
-apt install -y libreoffice evince scratch thonny geany idle3 arduino vlc pulseaudio lxterminal gedit catfish mousepad ristretto inkscape claws-mail firefox-esr catfish python3-opencv python3-pandas python3-numpy python3-pil ls2k-unlock-io python3-adafruit-blinka build-essential
+apt install -y task-lxde-desktop plymouth fonts-wqy-zenhei libreoffice evince scratch thonny geany idle3 arduino vlc pulseaudio lxterminal gedit catfish mousepad ristretto inkscape claws-mail firefox-esr catfish python3-opencv python3-pandas python3-numpy python3-pil ls2k-unlock-io python3-adafruit-blinka build-essential
 
 replace_apt_cacher_source
--- configs/mips64el-ls2k-lxde/setup/setup.sh
+++ configs/mips64el-ls2k-lxde/setup/setup.sh
@@ -7,4 +7,7 @@ source "$HOOK_FUNCTIONS"
 basic_setup
 enable_splash
 run_loongbian_workarounds
+
+apt install -y task-lxde-desktop plymouth fonts-wqy-zenhei
+
 replace_apt_cacher_source
